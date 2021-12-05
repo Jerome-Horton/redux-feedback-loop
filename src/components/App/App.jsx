d539aa5183bf3897f1397c3195e659f9f0c904f1
Jerome-Horton <riqui.howard@gmail.com>
2021-12-04 22:28:43 -0600

    Established & imported HomePage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
+// Import Components
+import HomePage from '../HomePage/HomePage'
+
 function App() {
 
   //The 
@@ -30,9 +33,12 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
+        <h1 className='App-title'>Welcome</h1>
+        <h3>Submit Your Feedback</h3>
         <h4>Don't forget it!</h4>
       </header>
+{/* import components in order */}
+      <HomePage />
     </div>
   );
 }
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useState } from 'react';
+
+// import dispatch
+
+function HomePage (){
+
+// Establish dispatch
+
+// Establish useState
+        // const  [feedback, setFeedback]
+
+// Create onClick function to go to next section of Survey
+// Implement dispatch when Reducers are set up
+// Also Implement useHistory
+
+
+    return(
+// build a wire frame  with Title and statement
+        <div>
+            <h2>Click the Button to begin.</h2>
+{/* // Button needs onClick functionality */}
+             <button>Start Survey</button>
+        </div>
+
+    )
+}
+
+
+
+
+export default HomePage;
