6ea0c7ebe34e97fa6020701db9890b90da1b98dd
Jerome-Horton <riqui.howard@gmail.com>
2021-12-04 21:27:51 -0600

    Established GET/route & fetch it.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  //The 
+    useEffect (() => {
+      console.log("in useEffect")
+      fetchFeedback();
+  }, []);
+
+  function fetchFeedback () {
+    console.log('in fetchFeedback', fetchFeedback)
+
+    axios({
+        method: "GET",
+        url: "/api/survey"
+    })
+      .then((response) => {
+          console.log("In GET response:", response.data);
+      })
+      .catch((error) => {
+          console.log("/api/survey GET error:", error);
+      });
+
+  }
+
 
   return (
     <div className='App'>
