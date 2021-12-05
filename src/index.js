54faaec986b34ad4232ddc03688121b82051c07c
Jerome-Horton <riqui.howard@gmail.com>
2021-12-05 13:19:12 -0600

    Installed react-redux & redux logger, and createdredux store

--- src/index.js
+++ src/index.js
@@ -4,5 +4,28 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Import Redux and createStore
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// import logger
+import logger from 'redux-logger';
+// Import Provider
+import { Provider } from 'react-redux';
+// Create Reducers to connect components
+
+// Create Store to hold App information
+const storeInstance = createStore(
+    combineReducers({
+       
+    }),
+    applyMiddleware(
+        logger),
+);
+
+
+// Add Store to ReactDom
+ReactDOM.render(
+    <Provider store={storeInstance}>
+        <App />
+    </Provider>, 
+document.getElementById('root'));
+    registerServiceWorker();
