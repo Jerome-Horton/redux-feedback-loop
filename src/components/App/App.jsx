679c3a637c3c1027cdb073bdba1ed26989cf8ba0
Jerome-Horton <riqui.howard@gmail.com>
2021-12-05 17:08:10 -0600

    dispatched GET /route and Imported Router Routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,8 +10,15 @@ import Support from '../Support/Support';
 import Comments from '../Comments/Comments';
 import ThankYou from '../ThankYou/ThankYou';
 
+// Import Router, route
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// Import dispatch
+import {useDispatch} from 'react-redux';
+
+
 function App() {
 
+  const dispatch = useDispatch;
   //The 
     useEffect (() => {
       console.log("in useEffect")
@@ -26,7 +33,11 @@ function App() {
         url: "/api/survey"
     })
       .then((response) => {
-          console.log("In GET response:", response.data);
+          // console.log("In GET response:", response.data);
+          dispatch({
+            type: 'SET_SURVEY',
+            payload: response.data
+          })
       })
       .catch((error) => {
           console.log("/api/survey GET error:", error);
@@ -37,18 +48,37 @@ function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Welcome</h1>
-        <h3>Submit Your Feedback</h3>
-        {/* <h4>Don't forget it!</h4> */}
-      </header>
+        <header className='App-header'>
+            <h1 className='App-title'>Welcome</h1>
+            <h3>Submit Your Feedback</h3>
+          {/* <h4>Don't forget it!</h4> */}
+        </header>
 {/* import components in order */}
-      <HomePage />
-      <Feelings />
-      <Understanding />
-      <Support />
-      <Comments />
-      <ThankYou />
+      <Router>
+        <Route exact path="/">
+              <HomePage />
+        </Route>
+
+        <Route exact path="/feelings">
+              <Feelings />
+        </Route>
+
+        <Route exact path="/feelings">
+              <Understanding />
+        </Route>
+
+          <Route exact path="/support">
+              <Support />
+          </Route>
+          
+          <Route exact path="/comments">
+              <Comments />
+          </Route>
+
+          <Route exact path="/">
+              <ThankYou />
+          </Route>
+      </Router>
     </div>
   );
 }
